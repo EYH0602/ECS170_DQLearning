0e189d59a381080407a0acfac060c293e55621ef
Ethan He <yfhe@ucdavis.edu>
2021-03-07 01:11:03 -0800

    start report

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # ECS170 Deep QLearning
+
+python test_dqn_pong.py model_pretrained.pth -g
--- dqn.py
+++ dqn.py
@@ -4,9 +4,12 @@ import matplotlib.pyplot as plt
 import torch
 import torch.nn as nn
 import torch.autograd as autograd
-import math, random
+import math
+import random
 USE_CUDA = torch.cuda.is_available()
-Variable = lambda *args, **kwargs: autograd.Variable(*args, **kwargs).cuda() if USE_CUDA else autograd.Variable(*args, **kwargs)
+Variable = lambda *args, **kwargs: autograd.Variable(
+    *args, **kwargs).cuda() if USE_CUDA else autograd.Variable(*args, **kwargs)
+
 
 class QLearner(nn.Module):
     def __init__(self, env, num_frames, batch_size, gamma, replay_buffer):
@@ -28,53 +31,48 @@ class QLearner(nn.Module):
             nn.Conv2d(64, 64, kernel_size=3, stride=1),
             nn.ReLU()
         )
-        
+
         self.fc = nn.Sequential(
             nn.Linear(self.feature_size(), 512),
             nn.ReLU(),
             nn.Linear(512, self.num_actions)
         )
-        
+
     def forward(self, x):
         x = self.features(x)
         x = x.view(x.size(0), -1)
         x = self.fc(x)
         return x
-    
+
     def feature_size(self):
-            return self.features(autograd.Variable(torch.zeros(1, *self.input_shape))).view(1, -1).size(1)
-    
+        return self.features(autograd.Variable(torch.zeros(1, *self.input_shape))).view(1, -1).size(1)
+
     def act(self, state, epsilon):
         if random.random() > epsilon:
-            state = Variable(torch.FloatTensor(np.float32(state)).unsqueeze(0), requires_grad=True)
+            state = Variable(torch.FloatTensor(
+                np.float32(state)).unsqueeze(0), requires_grad=True)
             # TODO: Given state, you should write code to get the Q value and chosen action
-        
-
-
-
-
-
+            action = torch.argmax(self(state)).item()
         else:
             action = random.randrange(self.env.action_space.n)
-        action = random.randrange(self.env.action_space.n)
         return action
 
     def copy_from(self, target):
         self.load_state_dict(target.state_dict())
 
-        
+
 def compute_td_loss(model, target_model, batch_size, gamma, replay_buffer):
     state, action, reward, next_state, done = replay_buffer.sample(batch_size)
 
-    state = Variable(torch.FloatTensor(np.float32(state)))
-    next_state = Variable(torch.FloatTensor(np.float32(next_state)).squeeze(1), requires_grad=True)
+    # state = Variable(torch.FloatTensor(np.float32(state)))
+    state = Variable(torch.FloatTensor(np.float32(state)).squeeze(1)) 
+    next_state = Variable(torch.FloatTensor(
+        np.float32(next_state)).squeeze(1), requires_grad=True)
     action = Variable(torch.LongTensor(action))
     reward = Variable(torch.FloatTensor(reward))
     done = Variable(torch.FloatTensor(done))
     # implement the loss function here
 
-
-    
     return loss
 
 
@@ -90,7 +88,6 @@ class ReplayBuffer(object):
 
     def sample(self, batch_size):
         # TODO: Randomly sampling data with specific batch size from the buffer
-
 
         return state, action, reward, next_state, done
 
